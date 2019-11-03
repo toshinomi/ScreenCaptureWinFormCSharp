e42c04672f5e201de48d87c2c22fc92b065df78a
toshinomi <tatsuya.oshinomi.0828@gmail.com>
2019-11-04 02:26:07 +0900

    プロジェクト ファイルを追加します。

---
+++ FormMain.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ScreenCaptureWinFormCSharp
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnMinimizedIcon = new System.Windows.Forms.Button();
+            this.btnCloseIcon = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BackColor = System.Drawing.Color.Black;
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(1, 1);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(418, 28);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Screen Capture";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(10, 38);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // btnMinimizedIcon
+            // 
+            this.btnMinimizedIcon.BackColor = System.Drawing.Color.Black;
+            this.btnMinimizedIcon.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMinimizedIcon.ForeColor = System.Drawing.Color.White;
+            this.btnMinimizedIcon.Location = new System.Drawing.Point(337, 2);
+            this.btnMinimizedIcon.Name = "btnMinimizedIcon";
+            this.btnMinimizedIcon.Size = new System.Drawing.Size(35, 25);
+            this.btnMinimizedIcon.TabIndex = 12;
+            this.btnMinimizedIcon.Text = "-";
+            this.btnMinimizedIcon.UseVisualStyleBackColor = false;
+            this.btnMinimizedIcon.Click += new System.EventHandler(this.OnClickBtnMinimizedIcon);
+            // 
+            // btnCloseIcon
+            // 
+            this.btnCloseIcon.BackColor = System.Drawing.Color.Black;
+            this.btnCloseIcon.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCloseIcon.ForeColor = System.Drawing.Color.White;
+            this.btnCloseIcon.Location = new System.Drawing.Point(382, 2);
+            this.btnCloseIcon.Name = "btnCloseIcon";
+            this.btnCloseIcon.Size = new System.Drawing.Size(35, 25);
+            this.btnCloseIcon.TabIndex = 11;
+            this.btnCloseIcon.Text = "×";
+            this.btnCloseIcon.UseVisualStyleBackColor = false;
+            this.btnCloseIcon.Click += new System.EventHandler(this.OnClickBtnClose);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(420, 450);
+            this.Controls.Add(this.btnMinimizedIcon);
+            this.Controls.Add(this.btnCloseIcon);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnMinimizedIcon;
+        private System.Windows.Forms.Button btnCloseIcon;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GrobalHook;
+
+namespace ScreenCaptureWinFormCSharp
+{
+    public partial class FormMain : Form
+    {
+        private Point m_mousePoint;
+        private static IntPtr m_hHook = IntPtr.Zero;
+        MouseHook.HookProcedureDelegate m_proc;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            lblTitle.MouseDown += new MouseEventHandler(OnMouseDownLblTitle);
+            lblTitle.MouseMove += new MouseEventHandler(OnMouseMoveLblTitle);
+
+            if (m_hHook == IntPtr.Zero)
+            {
+                m_proc = new MouseHook.HookProcedureDelegate(MouseHookProc);
+                SetMouseHook(m_proc);
+            }
+            else
+            {
+                RemoveMouseHook();
+            }
+        }
+
+        private void OnMouseDownLblTitle(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                m_mousePoint = new Point(e.X, e.Y);
+            }
+
+            return;
+        }
+
+        private void OnMouseMoveLblTitle(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                this.Left += e.X - m_mousePoint.X;
+                this.Top += e.Y - m_mousePoint.Y;
+            }
+
+            return;
+        }
+
+        private bool SetMouseHook(MouseHook.HookProcedureDelegate _proc)
+        {
+            bool bResult = true;
+
+            using (Process process = Process.GetCurrentProcess())
+            using (ProcessModule processModule = process.MainModule)
+            {
+                m_hHook = MouseHook.SetWindowsHookEx(MouseHook.WH_MOUSE_LL, _proc, MouseHook.GetModuleHandle(processModule.ModuleName), 0);
+                if (m_hHook == IntPtr.Zero)
+                {
+                    {
+                        MessageBox.Show("SetWindowsHookEx Failed.");
+                        bResult = false;
+                    }
+                }
+            }
+
+            return bResult;
+        }
+
+        private void RemoveMouseHook()
+        {
+            if (MouseHook.UnhookWindowsHookEx(m_hHook) == false)
+            {
+                MessageBox.Show("UnhookWindowsHookEx Failed.");
+            }
+
+            return;
+        }
+
+        private IntPtr MouseHookProc(int _nCode, IntPtr _wParam, IntPtr _lParam)
+        {
+            if (_nCode >= 0)
+            {
+                MouseHook.MouseHookStruct mouseHookStruct = (MouseHook.MouseHookStruct)Marshal.PtrToStructure(_lParam, typeof(MouseHook.MouseHookStruct));
+                if (mouseHookStruct != null && mouseHookStruct.pt.x > 0 && mouseHookStruct.pt.y > 0)
+                {
+                    String strText = "Screen Capture" + "  x = " + mouseHookStruct.pt.x.ToString("d") + " : y = " + mouseHookStruct.pt.y.ToString("d");
+                    lblTitle.Text = strText;
+
+                    Bitmap bitmap = new Bitmap(400, 400);
+                    Graphics graphics = Graphics.FromImage(bitmap);
+                    graphics.CopyFromScreen(mouseHookStruct.pt.x - 200, mouseHookStruct.pt.y - 200, 0, 0, bitmap.Size);
+                    graphics.Dispose();
+
+                    pictureBox.Image = bitmap;
+                    pictureBox.Refresh();
+                }
+            }
+
+            return MouseHook.CallNextHookEx(m_hHook, _nCode, _wParam, _lParam);
+        }
+
+        private void OnClickBtnClose(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Close the application ?", "Question", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2);
+            if (result == DialogResult.OK)
+            {
+                this.Close();
+            }
+
+            return;
+        }
+
+        private void OnClickBtnMinimizedIcon(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+
+            return;
+        }
+    }
+}
